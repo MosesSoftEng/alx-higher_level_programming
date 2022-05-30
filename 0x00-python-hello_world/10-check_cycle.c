fe5ef510b7d5ba0a6a86c707d0d3d5149f7c2262
MosesSoftEng <moses.soft.eng@gmail.com>
2022-05-30 22:28:08 +0300

    Code update to 10-check_cycle.c, lists.h

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,11 @@
-/*
- * File: 10-check_cycle.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - Checks if a singly-linked list contains a cycle.
+ * check_cycle - Checks if a singly-linked list contains a cycle
  * @list: A singly-linked list.
  *
- * Return: If there is no cycle - 0.
- *         If there is a cycle - 1.
+ * Return: 0 if @list has a cycle, otherwise 1
  */
 int check_cycle(listint_t *list)
 {
