196a1ecf1d3faf7a401a37fb1a20d7080423322f
Moses Mwangi <ngigemoses1@outlook.com>
2022-06-28 17:53:23 +0300

    Attemt task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+
+"""A Rectangle class."""
+
+
+class Rectangle:
+    """A rectangle."""
+    pass
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,3 +1,5 @@
 # 0x08. Python - More Classes and Objects
 
 # Tasks
+
+## 0-rectangle.py
