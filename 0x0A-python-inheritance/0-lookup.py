19a27fcdfb5d38ad87a43cd595e200016cb8ac01
Moses Mwangi <ngigemoses1@outlook.com>
2022-07-04 20:10:19 +0300

    Task 0 Attempt 6

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+"""Defines a function that lookup an object attribute"""
 
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
+    """Return a list of an object's available attributes"""
     return (dir(obj))
