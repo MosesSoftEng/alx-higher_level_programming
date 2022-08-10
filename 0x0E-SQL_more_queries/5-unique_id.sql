83d0862cd4437d448ffbe6d256a3cf3f9d58b3a6
Moses Mwangi <ngigemoses1@outlook.com>
2022-08-10 15:26:15 +0300

    Task 5 attempt 2

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- Script that creates the table id_not_null on your MySQL server.
 
 -- Create table
-CREATE TABLE IF NOT EXISTS `id_not_null` (
+CREATE TABLE IF NOT EXISTS `unique_id` (
   -- DEFAULT, set default value to 1 --
   `id` INT DEFAULT 1 UNIQUE,
   `name` VARCHAR(256)
