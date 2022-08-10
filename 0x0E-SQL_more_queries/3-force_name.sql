8585f6ea738c0124bf9afb57ae41010ac5a7c85e
Moses Mwangi <ngigemoses1@outlook.com>
2022-08-10 15:12:43 +0300

    Task 3 attempt 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -3,6 +3,6 @@
 -- Create table
 CREATE TABLE IF NOT EXISTS `force_name` (
   `id` INT, 
-  --NOT NULL, Constrain column value to be not NULL--
+  -- NOT NULL, Constrain column value to be not NULL --
   `name` VARCHAR(256) NOT NULL
 );
