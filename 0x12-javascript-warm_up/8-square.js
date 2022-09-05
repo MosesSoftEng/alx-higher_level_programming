cbbf67695c18f1c20b3241e56994b47420473572
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-05 20:35:18 +0300

    Task 8m attempt 2

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 
-const num = Number(process.argv[2]);
+const size = Number(process.argv[2]);
 
-if (num) {
-  let line = '';
-
-  for (let i = 0; i < num; i++) {
-    for (let j = 0; j < num; j++) {
-      line += 'x';
-    }
-
-    if(i < num - 1) line += '\n'
+if (size) {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let c = 0; c < size; c++) line += 'X';
+    console.log(line);
   }
-
-  if(num > 0) console.log(line);
 } else {
   console.log('Missing size');
 }
