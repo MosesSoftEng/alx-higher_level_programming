1616ed10a892131437a7225c270536672560a4a0
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-22 18:58:45 +0300

    Remove whole word option

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the size of the body of the response in bytes
-curl -Is "$1" | grep -w 'Content-Length' | cut -d ' ' -f 2
+curl -Is "$1" | grep 'Content-Length' | cut -d ' ' -f 2
