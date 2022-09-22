173b0423ffca9829ae04967027af0fd3d711c7ad
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-22 19:38:19 +0300

    Replace fixed URL with command argument

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept for a giben URL
-curl -sILX OPTIONS eiwriter.com | grep -i 'Allow:' | cut -f2- -d' '
+curl -sILX OPTIONS "$1" | grep -i 'Allow:' | cut -f2- -d' '
