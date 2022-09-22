2dfb78518e634830def413d56f38c08a6e6789dd
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-22 18:31:30 +0300

    Task 0 attempt 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 # Displays the size of the body of the response in bytes
 
-# Size of body is saved in Content-Length of HTTP server response header
-#   message in bytes
-#
-# $1  first command argument
-# curl -I   Show document info only
-# curl -s   Silent mode, disable progression display
-# grep 'Content-Length'   Get line with the string Content-Length
-# |   pipe redirection, Redirect from one command to another.
-# cut -d ' '    split string by whitespace delimiter
-# ......-f 2     get second split
-
-curl -Is "$1" | grep 'Content-Length' | cut -d ' ' -f 2
+curl -Is "$1" | grep -w 'Content-Length' | cut -d ' ' -f 2
