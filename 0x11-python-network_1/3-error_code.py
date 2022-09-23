b8483cd8028c7eead66164cf365b488c470471aa
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-23 14:24:09 +0300

    Decode cody reponse t otf-8

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
             # Get response headers
             body = response.read()
 
-            print(body)
+            print(body.decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
