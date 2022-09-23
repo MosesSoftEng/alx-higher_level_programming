bcaaecffb00968eb5c77cef3e743bb82533b9805
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-23 14:22:18 +0300

    Task 3 attempt

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Display value of X-Request-Id variable found in the header of a response."""
+import sys
+import urllib.request
+import urllib.error
+
+
+# Evaluate if running this script directly, will not work if imported.
+if __name__ == "__main__":
+    # Get first command argument
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            # Get response headers
+            body = response.read()
+
+            print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
