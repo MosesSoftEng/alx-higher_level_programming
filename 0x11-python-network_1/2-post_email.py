5cb3da4556725dad96f53cb6d0b5fcc10b172946
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-23 14:08:23 +0300

    Return comments

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,14 +6,18 @@ import urllib.request
 
 # Evaluate if running this script directly, will not work if imported.
 if __name__ == "__main__":
+    # Get first command argument
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
 
+    # Prepare request
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     request = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(request) as response:
+        # Get response body
         body = response.read()
 
+        # Decode in utf-8 and print
         print(body.decode("utf-8"))
