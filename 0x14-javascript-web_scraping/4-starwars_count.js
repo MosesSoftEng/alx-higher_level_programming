bce8612ed42ac8621bc40fde50cabd66b063c84c
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-20 10:42:28 +0300

    Add script that prints character appearance

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* Script that prints the number of movies where
+ * the character "Wedge Antilles" is present.
+ */
+// Import modules
+const request = require('request');
+
+// Get command arguments
+const url = process.argv[2];
+
+// Send get request
+request(url, function (error, response, body) {
+    const films = JSON.parse(body).results;
+    let character_movie_count = 0;
+
+    if (!error) {
+        for (let i = 0; i < films.length; i++) {
+            const film = films[i];
+
+            if(film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')){
+                character_movie_count++;
+            }
+        }
+    }
+    console.log(character_movie_count);
+});
