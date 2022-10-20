74a2d1fafcf63b1bde926dfbe3d8e4dd4791215d
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-20 12:29:57 +0300

    Add script to get film characters names

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* Script that prints all characters of a Star Wars movie. */
+// Import modules
+const request = require('request');
+
+// Get command arguments
+const movieID = process.argv[2];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+
+// Send request
+request(url, function (error, response, body) {
+  if (!error) {
+    const film = JSON.parse(body);
+
+    film.characters.forEach(character => {
+        request(character, function (error, response, body) {
+            if (!error) {
+                console.log(JSON.parse(body).name);
+            }
+          });
+    });
+  }
+});
