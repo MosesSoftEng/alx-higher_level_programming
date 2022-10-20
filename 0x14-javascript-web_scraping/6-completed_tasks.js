36463ec848b38240a8d437bd620145691e369b21
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-20 12:10:57 +0300

    Add script to count user completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+/* Script that computes the number of tasks completed by user id. */
+// Import modules
+const request = require('request');
+
+// Get command arguments
+const url = process.argv[2];
+
+// Send get request
+request(url, function (error, response, body) {
+  if (!error) {
+    let userTaskData = {};
+
+    const todos = JSON.parse(body);
+
+    // Loop each todo
+    todos.forEach(todo => {
+        console.log(todo);
+
+        if(!todo.completed){
+            return;
+        }
+
+
+        if(userTaskData[todo.userId]) {
+            userTaskData[todo.userId] = userTaskData[todo.userId] + 1;
+        } else {
+            userTaskData[todo.userId] = 1
+        }
+    });
+
+
+    console.log(userTaskData);
+  }
+});
