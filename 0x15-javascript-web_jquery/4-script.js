b97f343cac425ec5c8380fee472639513f47d77c
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-26 18:51:11 +0300

    Code refactor

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -2,10 +2,10 @@ $('DIV#toggle_header').click(() => {
   const header = $('header');
 
   if(header.hasClass('green')) {
-    header.removeClass('green')
+    header.removeClass('green');
     header.addClass('red');
   } else {
-    header.removeClass('red')
+    header.removeClass('red');
     header.addClass('green');
   }
 });
